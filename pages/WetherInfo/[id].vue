002e88c397332b947823ea3fa87d16cfb8fec3fd
s.nakamura <shotenba@gmail.com>
2024-01-08 09:46:59 +0900

    asyncDataの書き方に修正

--- pages/WetherInfo/[id].vue
+++ pages/WetherInfo/[id].vue
@@ -4,9 +4,33 @@ const route = useRoute();
 const cityList = useState<Map<number, City>>("cityList");
 const selectedCity = computed((): City => {
   const id = Number(route.params.id);
-  return cityList.value.get(id);
+  return cityList.value.get(id) as City;
 });
 const weatherDescription = ref("");
+const asyncData = await useAsyncData(
+  `/WeatherInfo/${route.params.id}`,
+  (): Promise<any> => {
+    const weatherInfoUrl = "https://api.openweathermap.org/data/2.5/weather";
+    const params: { lang: string; q: string; appid: string } = {
+      lang: "ja",
+      q: selectedCity.value.q,
+      appid: "c7ecfc76ab4439eecf7d37bbce808329",
+    };
+    const queryParams = new URLSearchParams(params);
+    const urlFull = `${weatherInfoUrl}?${queryParams}`;
+    console.log(urlFull);
+    const response = $fetch(urlFull);
+    return response;
+  },
+  {
+    pick: ["weather"],
+  }
+);
+const data = asyncData.data;
+
+const weatherArray = data.value.weather;
+const weather = weatherArray[0];
+weatherDescription.value = weather.description;
 </script>
 
 <template>
